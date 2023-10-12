fe5f8c2e2a90bd16a7643905090c520fe9dbce92
Samir_Allahverdiyev <smrallahverdieff@gmail.com>
2023-10-12 23:24:18 +0200

    k8s yamls

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
 FROM adoptopenjdk/openjdk11:alpine
-RUN addgroup -S spring && adduser -S spring -G spring
-USER spring:spring
-VOLUME /tmp
-ARG JAR_FILE
-ADD ${JAR_FILE} /app/app.jar
-EXPOSE 8080
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app/app.jar"]
+COPY target/*.jar /app/app.jar
+
+EXPOSE 8081
+ENTRYPOINT ["java","-jar","/app/app.jar"]
 
 
--- src/main/java/com/example/JenkinsDemoApplication.java
+++ src/main/java/com/example/JenkinsDemoApplication.java
@@ -2,11 +2,17 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDemoApplication {
 
-
+    @GetMapping
+    public String get() {
+        return "Hello World!";
+    }
 
     //TODO: application.yaml
     //TODO: liquibase
